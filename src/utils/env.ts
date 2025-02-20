aa3baf1eedcb93c178c53582362a6104faf81291
albqvictor1508 <albq.victor@gmail.com>
2025-02-20 10:09:31 -0300

    chore: add docker compose file with postgres and redis envs

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,5 +1,7 @@
-import { cleanEnv, str, num } from "envalid";
+import { cleanEnv, num, url } from "envalid";
 
 export const env = cleanEnv(process.env, {
 	API_PORT: num({ default: 3333 }),
+	POSTGRES_URL: url(),
+	REDIS_URL: url(),
 });
