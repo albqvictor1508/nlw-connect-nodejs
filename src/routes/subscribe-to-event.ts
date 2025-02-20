b63c3097812717b2531d0a9877192aac74d4a6ef
albqvictor1508 <albq.victor@gmail.com>
2025-02-20 01:08:13 -0300

    add type flag to import

--- src/routes/subscribe-to-event.ts
+++ src/routes/subscribe-to-event.ts
@@ -1,23 +1,27 @@
-import { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
+import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import z from "zod";
 
 export const subscribeToEventRoute: FastifyPluginAsyncZod = async (app) => {
-    app.post("/api/subscriptions", {
-        schema: {
-            summary: "subscribers someone to the event",
-            tags: ["subscription"],
-            description: "msr msr msr",
-            body: z.object({
-                name: z.string(),
-                email: z.string().email(),
-            }),
-            response: {
-                201: z.object({
-                    msg: z.string()
-                })
-            }
-        }
-    }, (request, reply) => {
-        reply.status(201).send({msg: "user created!"});
-    })
-}
+	app.post(
+		"/api/subscriptions",
+		{
+			schema: {
+				summary: "subscribers someone to the event",
+				tags: ["subscription"],
+				description: "msr msr msr",
+				body: z.object({
+					name: z.string(),
+					email: z.string().email(),
+				}),
+				response: {
+					201: z.object({
+						msg: z.string(),
+					}),
+				},
+			},
+		},
+		(request, reply) => {
+			reply.status(201).send({ msg: "user created!" });
+		},
+	);
+};
