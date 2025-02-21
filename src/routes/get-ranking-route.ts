0a42e6a69348931ed2bec9421b02b34e03a974a2
albqvictor1508 <albq.victor@gmail.com>
2025-02-21 00:16:44 -0300

    error: some errors on project and WIK

---
+++ src/routes/get-ranking-route.ts
@@ -0,0 +1,30 @@
+import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { getRanking } from "../functions/get-ranking";
+
+export const getRankingRoute: FastifyPluginAsyncZod = async (app) => {
+	app.get(
+		"/api/ranking",
+		{
+			schema: {
+				summary: "Get ranking",
+				tags: ["referral"],
+				params: z.object({
+					subscriberId: z.string(),
+				}),
+				response: {
+					200: z.object({
+						id: z.string(),
+						name: z.string(),
+						score: z.number(),
+					}),
+				},
+			},
+		},
+		async () => {
+			const { ranking } = await getRanking();
+
+			return { ranking };
+		},
+	);
+};
